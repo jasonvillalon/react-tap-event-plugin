cd2676140d4d6d5d8de57e133b1b3b82be67bca2
Jason Villalon <jasonvillalon@gmail.com>
2015-06-05 09:52:06 +0800

    Update injectTapEventPlugin.js
    removed .js

--- src/injectTapEventPlugin.js
+++ src/injectTapEventPlugin.js
@@ -3,7 +3,7 @@ module.exports = function injectTapEventPlugin () {
   React.initializeTouchEvents(true);
 
   require('react/lib/EventPluginHub').injection.injectEventPluginsByName({
-    "ResponderEventPlugin": require('./ResponderEventPlugin.js'),
-    "TapEventPlugin":       require('./TapEventPlugin.js')
+    "ResponderEventPlugin": require('./ResponderEventPlugin'),
+    "TapEventPlugin":       require('./TapEventPlugin')
   });
 };
